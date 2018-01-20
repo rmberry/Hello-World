e74d9adfd94d5a0caaa33dfca9d2a6db2b5eebab
Ryan Berry <35620592+rmberry@users.noreply.github.com>
2018-01-19 23:57:00 -0600

    Create Hello_World.c
    Program that prints my name "Ryan Berry" using the printf function and then exits.

---
+++ Hello_World.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * Basic Hello World program in C
+ * Prints "Ryan Berry" instead of "Hello World"
+ * Then the program exits
+ */
+
+int main(int argc, char **argv); {
+
+  printf("Ryan Berry\n");
+
+  return 0;
+}
